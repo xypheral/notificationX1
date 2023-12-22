bff31ee0f11c14887dd2d4eb048604935b6c2ee0
Wei Han <noelng26@gmail.com>
2023-12-22 15:35:36 +0800

    update

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import messaging from '@react-native-firebase/messaging';
+import React, { useEffect } from 'react';
 export default function App() {
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  
+    return enabled; // Return the result
+  };
+  
+  useEffect(() => {
+    const checkPermission = async () => {
+      const permission = await requestUserPermission();
+      if (permission) {
+        messaging()
+          .getToken()
+          .then(token => console.log(token));
+      }
+    };
+    checkPermission();
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
